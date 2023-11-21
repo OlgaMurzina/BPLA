fdccb5fd484acbb82b0f0364c107cd63e9b64396
OlgaMurzina <74879104+OlgaMurzina@users.noreply.github.com>
2023-11-21 22:37:17 +0300

    load-download to Pythonanywhere.com
    Файл загрузки-выгрузки в облако

---
+++ load_dounload_cloud.py
@@ -0,0 +1,40 @@
+import json
+from random import uniform
+import requests
+
+username = 'olmur'
+token = '0a37138668bbad146a9bcdd77640c90368b2cdbf'
+path = '/home/olmur/quadrocopter'
+content2 = {  'vx': uniform(0, 3),
+              'vy': uniform(0, 3),
+              'vz': uniform(0, 3)}
+content = json.dumps(content2)
+
+response = requests.post(
+    'https://www.pythonanywhere.com/api/v0/user/{username}/files/path{path}/sensor.in'.format(
+        username=username, path=path ),
+        headers={'Authorization': 'Token {token}'.format(token=token)},
+        files = {'content': content}
+)
+
+if response.status_code in [200, 201]:
+    print('File uploaded')
+else:
+    print('Got unexpected status code {}: {!r}'.format(response.status_code, response.content))
+
+response = requests.get('https://www.pythonanywhere.com/api/v0/user/{username}/files/path{path}/sensor.in'.format(
+        username=username,
+        path=path ),
+        headers={'Authorization': 'Token {token}'.format(token=token)})
+
+try:
+    file1 = response
+    content1 = json.loads(file1.content)
+
+    if content1 == content2:
+        print("Файлы идентичны")
+    else:
+        print("Файлы имеют различия")
+
+except Exception as e:
+    print(e)
